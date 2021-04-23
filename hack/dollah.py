9531eb0237473683022060218aa2a2382d1ce0ef
John Mulligan <jmulligan@redhat.com>
2021-04-23 11:15:39 -0400

    x demo: first pass at demo script
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

---
+++ hack/dollah.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+
+import argparse
+import shlex
+import sys
+import random
+import time
+import getpass
+import tty
+import termios
+import os
+
+DOLLAH_WAIT = True
+DOLLAH_FAST = False
+DOLLAH_PROMPT = '[demo]$ '
+
+if os.environ.get("DOLLAH_WAIT") == "no":
+    DOLLAH_WAIT = False
+
+if os.environ.get("DOLLAH_FAST") == "yes":
+    DOLLAH_FAST = True
+
+if os.environ.get("DOLLAH_PROMPT", "") != "":
+    DOLLAH_PROMPT = os.environ.get("DOLLAH_PROMPT", "")
+
+
+def prompt(dest=sys.stdout):
+    dest.write(DOLLAH_PROMPT)
+    dest.flush()
+    wait()
+
+
+def wait():
+    if os.environ.get("DOLLAH_WAIT") == "no":
+        return
+    fd = sys.stdin.fileno()
+    before = termios.tcgetattr(fd)
+    tty.setraw(fd)
+    sys.stdin.read(1)
+    termios.tcsetattr(fd, termios.TCSADRAIN, before)
+
+
+def fast_sleep(seconds):
+    time.sleep(seconds / 4)
+
+
+def typewrite(s, dest=sys.stdout):
+    if DOLLAH_FAST:
+        _sleep = fast_sleep
+    else:
+        _sleep = time.sleep
+    lslp = 0
+    for c in s:
+        _sleep(0.015 * random.randint(1, 10))
+        if lslp > 5:
+            _sleep(0.002 * random.randint(2, 20))
+        if random.randint(1,10) == 1:
+            _sleep(0.1)
+        if c == " ":
+            _sleep(0.2)
+            lslp = 0
+        else:
+            lslp += 1
+        dest.write(c)
+        dest.flush()
+        if c == " ":
+            _sleep(0.03 * random.randint(1, 10))
+
+
+def read(src):
+    out = []
+    for line in src:
+        p = shlex.split(line, posix=True)
+        cmd = list(p)
+        if cmd:
+            out.append(cmd)
+    return out
+
+
+def present(cmd):
+    prompt()
+    typewrite(shlex.join(cmd))
+    sys.stdout.write('\n')
+
+def main():
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument('source')
+    #cli = parser.parse_args()
+
+
+    #with open(cli.source) as fh:
+    #    cmds = read(fh)
+    #for cmd in cmds:
+    #    present(cmd)
+    present(sys.argv[1:])
+
+
+if __name__ == '__main__':
+    main()
---
+++ sxp-demo/demo.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+section() {
+    echo "###" "$@" "###"
+}
+
+say() {
+    demo "echo" "$@"
+}
+
+demo() {
+    ./hack/dollah.py "$@"
+    "$@"
+}
+
+must() {
+    "$@" || {
+        echo "ERROR"
+        exit 1
+    }
+}
+
+manual() {
+    PS1='[show]$ ' bash --norc
+}
+
+ns=samba-operator-system
+
+section "Deploying the operator"
+
+# deploy the operator (manually, via makefile)
+must demo make deploy
+
+must demo kubectl -n "$ns" get pods
+
+# deploy the smb client pod, etc.
+must demo kubectl -n "$ns" apply -f ./sxp-demo/client.yaml
+
+must demo kubectl -n "$ns" get pods
+
+
+section "Deploying the Custom Resources - Without Active Directory"
+
+# show some files
+manual
+
+must demo kubectl -n "$ns" apply -f ./sxp-demo/ugsec1.yaml
+must demo kubectl -n "$ns" apply -f ./sxp-demo/ugshare1.yaml
+
+demo kubectl -n "$ns" get pods -w
+
+say "let us access the share"
+demo kubectl -n "$ns" exec -it smbclient -- smbclient -U 'sambauser%letm31n_pls' "//first.$ns.svc.cluster.local/First Share"
+
+say "what do we see within the container?"
+demo kubectl -n "$ns" exec -it deploy/first -- bash
+
+section "Deploying the Custom Resources - With Active Directory"
+
+# show some files
+manual
+
+
+must demo kubectl -n "$ns" apply -f ./sxp-demo/adsecurity.yaml
+must demo kubectl -n "$ns" apply -f ./sxp-demo/adshare1.yaml
+must demo kubectl -n "$ns" apply -f ./sxp-demo/adshare2.yaml
+
+demo kubectl -n "$ns" get pods -w
+
+
+demo kubectl -n "$ns" exec -it smbclient -- smbclient -U 'DOMAIN1/bwayne%1115Rose.' "//users.$ns.svc.cluster.local/Users"
+demo kubectl -n "$ns" exec -it smbclient -- smbclient -U 'DOMAIN1/bwayne%1115Rose.' "//users.domain1.sink.test/Users"
