db8d2ec81036cf97025c40fc816b833bdd69b9d0
John Mulligan <jmulligan@redhat.com>
2021-04-29 14:12:32 -0400

    xxx: clean up demo script: namspaces, pause points
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- sxp-demo/demo.sh
+++ sxp-demo/demo.sh
@@ -47,15 +47,21 @@ ns=samba-operator-system
 
 section "Deploying the operator"
 
+must demo kubectl get namespaces
+
 # deploy the operator (manually, via makefile)
 must demo make deploy
 
-must demo kubectl -n "$ns" get pods
+must demo kubectl get namespaces
+
+must demo kubectl config set-context --current --namespace=samba-operator-system
+
+must demo kubectl get pods
 
 # deploy the smb client pod, etc.
-must demo kubectl -n "$ns" apply -f ./sxp-demo/client.yaml
+must demo kubectl apply -f ./sxp-demo/client.yaml
 
-must demo kubectl -n "$ns" get pods
+demo kubectl get pods -w
 
 
 section "Deploying the Custom Resources - Without Active Directory"
@@ -63,16 +69,16 @@ section "Deploying the Custom Resources - Without Active Directory"
 # show some files
 manual
 
-must demo kubectl -n "$ns" apply -f ./sxp-demo/ugsec1.yaml
-must demo kubectl -n "$ns" apply -f ./sxp-demo/ugshare1.yaml
+must demo kubectl apply -f ./sxp-demo/ugsec1.yaml
+must demo kubectl apply -f ./sxp-demo/ugshare1.yaml
 
-demo kubectl -n "$ns" get pods -w
+demo kubectl get pods -w
 
-say "let us access the share"
-demo kubectl -n "$ns" exec -it smbclient -- smbclient -U 'sambauser%letm31n_pls' "//first.$ns.svc.cluster.local/First Share"
+say "we can now access the share"
+demo kubectl exec -it smbclient -- smbclient -U 'sambauser%letm31n_pls' "//first.$ns.svc.cluster.local/First Share"
 
 say "what do we see within the container?"
-demo kubectl -n "$ns" exec -it deploy/first -- bash
+demo kubectl exec -it deploy/first -- bash
 
 section "Deploying the Custom Resources - With Active Directory"
 
@@ -80,28 +86,29 @@ section "Deploying the Custom Resources - With Active Directory"
 manual
 
 
-must demo kubectl -n "$ns" apply -f ./sxp-demo/adsecurity.yaml
-must demo kubectl -n "$ns" apply -f ./sxp-demo/adshare1.yaml
-must demo kubectl -n "$ns" apply -f ./sxp-demo/adshare2.yaml
+must demo kubectl apply -f ./sxp-demo/adsecurity.yaml
+must demo kubectl apply -f ./sxp-demo/adshare1.yaml
+must demo kubectl apply -f ./sxp-demo/adshare2.yaml
 
-demo kubectl -n "$ns" get pods -w
+demo kubectl get pods -w
 
 
-if ! kubectl -n "$ns" exec -it smbclient -- smbclient -U 'DOMAIN1/bwayne%1115Rose.' "//users.$ns.svc.cluster.local/Users" -c "ls" >/dev/null ; then
+if ! kubectl exec -it smbclient -- smbclient -U 'DOMAIN1/bwayne%1115Rose.' "//users.$ns.svc.cluster.local/Users" -c "ls" >/dev/null ; then
     err_users=true
 fi
 
-if ! kubectl -n "$ns" exec -it smbclient -- smbclient -U 'DOMAIN1/bwayne%1115Rose.' "//users.domain1.sink.test/Users" -c "ls" >/dev/null ; then
+if ! kubectl exec -it smbclient -- smbclient -U 'DOMAIN1/bwayne%1115Rose.' "//users.domain1.sink.test/Users" -c "ls" >/dev/null ; then
     err_archive=true
 fi
 
 if [ "$err_users" = "true" -o "$err_archive" = "true" ]; then
     say "we can dive deeper"
 else
-    say "we can now use the shares - start with smbclient in cluster"
-    demo kubectl -n "$ns" exec -it smbclient -- smbclient -U 'DOMAIN1/bwayne%1115Rose.' "//users.$ns.svc.cluster.local/Users"
+    say "now to copy some files - using smbclient"
+    demo kubectl exec -it smbclient -- smbclient -U 'DOMAIN1/ckent%1115Rose.' "//users.$ns.svc.cluster.local/Users"
 fi
 
+say "time to access the shares from outside the cluster"
 manual
 
 all_done
