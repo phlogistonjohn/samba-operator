fa0f05f5c7ddb304de90144d1c9a99d173bb91a3
John Mulligan <jmulligan@redhat.com>
2022-07-29 20:10:46 -0400

    xxx: fill out compatible check
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- internal/planner/compatible.go
+++ internal/planner/compatible.go
@@ -4,17 +4,32 @@ package planner
 
 import (
 	"fmt"
+
+	"k8s.io/apimachinery/pkg/types"
 )
 
 type IncompatibleInstanceError struct {
 	current  string
 	existing string
+	reason string
 }
 
 func (iie IncompatibleInstanceError) Error() string {
-	return fmt.Sprintf("Share resource %s is incompatible with %s",
+	return fmt.Sprintf("Share resource %s is incompatible with %s: %s",
 		iie.current,
-		iie.existing)
+		iie.existing,
+		iie.reason)
+}
+
+func incompatible(
+	current, existing InstanceConfiguration,
+	reason string) IncompatibleInstanceError {
+	// ---
+	return IncompatibleInstanceError{
+		current: current.SmbShare.Name,
+		existing: existing.SmbShare.Name,
+		reason: reason,
+	}
 }
 
 // CheckCompatible returns an error if the instance configurations are
@@ -24,8 +39,46 @@ func CheckCompatible(current, existing InstanceConfiguration) error {
 	// This returns an error rather than a boolean because in the future
 	// we can choose to add details about what is incompatible to
 	// the specific error type.
-	return IncompatibleInstanceError{
-		current.SmbShare.Name,
-		existing.SmbShare.Name,
+	if current.SmbShare.Namespace != existing.SmbShare.Namespace {
+		return incompatible(current, existing, "namespaces differ")
 	}
+	if current.SmbShare.Spec.Storage.Pvc.Name != existing.SmbShare.Spec.Storage.Pvc.Name {
+		return incompatible(current, existing,
+			"PersistentVolumeClaim name mismatch")
+	}
+	if current.SmbShare.Spec.SecurityConfig != existing.SmbShare.Spec.SecurityConfig {
+		return incompatible(current, existing,
+			"security config name mismatch")
+	}
+	if current.SmbShare.Spec.CommonConfig != existing.SmbShare.Spec.CommonConfig {
+		return incompatible(current, existing,
+			"common config name mismatch")
+	}
+
+	// additional checks
+	var uid1, uid2 types.UID
+	if current.SecurityConfig != nil {
+		uid1 = current.SecurityConfig.UID
+	}
+	if existing.SecurityConfig != nil {
+		uid2 = existing.SecurityConfig.UID
+	}
+	if uid1 != uid2 {
+		return incompatible(current, existing,
+			"security configuration resources differ")
+	}
+
+	uid1, uid2 = "", ""
+	if current.CommonConfig != nil {
+		uid1 = current.CommonConfig.UID
+	}
+	if existing.CommonConfig != nil {
+		uid2 = existing.CommonConfig.UID
+	}
+	if uid1 != uid2 {
+		return incompatible(current, existing,
+			"common configuration resources differ")
+	}
+
+	return nil
 }
