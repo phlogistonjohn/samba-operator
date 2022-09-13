094143357bd271fa0a48d34a3eb7de5f84334d61
John Mulligan <jmulligan@redhat.com>
2022-09-13 09:51:25 -0400

    resources: no need for this checker exception any more
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- internal/resources/smbshare.go
+++ internal/resources/smbshare.go
@@ -106,7 +106,6 @@ func (m *SmbShareManager) Process(
 }
 
 // Update should be called when a SmbShare resource changes.
-// nolint:funlen
 func (m *SmbShareManager) Update(
 	ctx context.Context,
 	instance *sambaoperatorv1alpha1.SmbShare) Result {
